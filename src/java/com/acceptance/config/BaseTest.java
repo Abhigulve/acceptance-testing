733d09d5b699ab3195782757058d5cf0bac8c4f9
vyankat70war <vyankat70war@gmail.com>
2018-03-20 21:07:56 +0530

    updated driver path based on OS

--- src/java/com/acceptance/config/BaseTest.java
+++ src/java/com/acceptance/config/BaseTest.java
@@ -13,9 +13,16 @@ public abstract class BaseTest
         browser = "chrome"; // chrome, ie, safari
         holdBrowserOpen = true;
 
+        String driver = "";
+        String os = System.getProperty("os.name");
+        if(os.contains("Windows")){
+            driver = "drivers/windows/chromedriver.exe";
+        }else if(os.contains("Ubuntu")){
+            driver = "drivers/ubuntu/chromedriver";
+        }
         //switch to local drivers folder using File.exists
        // System.setProperty("webdriver.gecko.driver", "/Users/mmckenzie/development/branches/radix-dash-all/acceptance-tests/drivers/geckodriver");
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", driver);
        // System.setProperty("webdriver.chrome.driver", "your/driver/path");
         //System.setProperty("webdriver.firefox.marionette", "/Users/mmckenzie/development/branches/radix-dash-all/acceptance-tests/drivers/geckodriver");
     }
